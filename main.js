71968481bc791fe44fb394e887022cecd5193b30
Jeffrey Kornfeld <jakornfeld@yahoo.com>
2021-02-04 20:25:51 -0600

    5 day forecast loop added

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var currentTemp = $("#current-temperature")
 var currentHumidity = $("#current-humidity")
 var currentWindSpeed = $("#current-wind-speed")
 
-//Displays the Weather
+// Displays the Weather
 function displayWeather(event) {
     event.preventDefault();
     // Pulls value of Search City form and puts it in the queryURL
@@ -16,20 +16,29 @@ function displayWeather(event) {
         method: "GET"
     }).then(function (response) {
         console.log(response);
-
-        //Displays City Name
+        // Displays City Name
         var dateToday = moment().format("l")
         $(currentCity).html(response.city.name + " (" + dateToday + ")");
-
-        //Converts Temp to Farenheit and displays 
+        // Converts Temp to Farenheit and displays 
         var tempF = (response.list[0].main.temp - 273.15) * 1.80 + 32;
         $(currentTemp).html(" " + (tempF).toFixed(2) + "&#8457");
-
-        //Displays Humidity
+        // Displays Humidity
         $(currentHumidity).html(" " + response.list[0].main.humidity + "%");
-
-        //Displays Wind Speed
+        // Displays Wind Speed
         $(currentWindSpeed).html(" " + response.list[0].wind.speed + "mph");
+
+
+        // 5 Day Forecast Loop
+        for (i=1; i<6;i++){
+            var dateNext = moment().add(i, 'days').format("l");
+            var temp = (response.list[i].main.temp - 273.15) * 1.80 + 32;
+            var humidity = response.list[i].main.humidity
+            $("#date"+i).html(" " + dateNext);
+            $("#temp"+i).html(" " + temp.toFixed(2) + "&#8457");
+            $("#humidity"+i).html(" " + humidity + "%");
+        }
+
+
     })
 }
 
