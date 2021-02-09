ebfa935340d1cd2869ca9cf98cbc40931fd35c8a
Jeffrey Kornfeld <jakornfeld@yahoo.com>
2021-02-08 18:21:26 -0600

    search adds cities to list element. Clear history removes all list elements

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var city = "";
+var savedCities = [];
 var searchCity = $("#search-city");
 var currentCity = $("#current-city");
 var currentTemp = $("#current-temperature")
@@ -27,21 +28,39 @@ function displayWeather(event) {
         // Displays Wind Speed
         $(currentWindSpeed).html(" " + response.list[0].wind.speed + "mph");
 
-
         // 5 Day Forecast Loop
-        for (i=1; i<6;i++){
+        for (i = 1; i < 6; i++) {
             var dateNext = moment().add(i, 'days').format("l");
             var temp = (response.list[i].main.temp - 273.15) * 1.80 + 32;
             var humidity = response.list[i].main.humidity
-            $("#date"+i).html(" " + dateNext);
-            $("#temp"+i).html(" " + temp.toFixed(2) + "&#8457");
-            $("#humidity"+i).html(" " + humidity + "%");
+            $("#date" + i).html(" " + dateNext);
+            $("#temp" + i).html(" " + temp.toFixed(2) + "&#8457");
+            $("#humidity" + i).html(" " + humidity + "%");
         }
 
+
+        // Add city to list of previous searches
+
+        console.log(cityName)
+        addToList(cityName)
 
     })
 }
 
+function addToList(c){
+    var listEl= $("<li>"+c.toUpperCase()+"</li>");
+    $(listEl).attr("class","list-group-item");
+    $(listEl).attr("data-value",c.toUpperCase());
+    $(".list-group").append(listEl);
+}
+
+function clearStorage() {
+    $(".list-group").empty();
+}
+
+
+
 
 //invokes Display Weather function on button click
 $("#search-button").on("click", displayWeather);
+$("#clear-history").on("click", clearStorage);
